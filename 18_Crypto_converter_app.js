e6e443b555f7a2db037fa80fb3c0062b6be47c2f
namanjeet <namanshuttler@gmail.com>
2024-07-30 23:06:30 +0530

    30.07.24

--- 18_Crypto_converter_app.js
+++ 18_Crypto_converter_app.js
@@ -1,11 +1,16 @@
 const BASE_URL = "https://api.coincap.io/v2/assets"
 
+// https://api.coincap.io/v2/assets/ethereum
+
 const dropdown = document.querySelectorAll(".all-select select")
 
 const btn = document.querySelector("form button")
 
 const fromCryptoCurr = document.querySelector(".from select")
-const toCryptoCurr = document.querySelector(".from select")
+
+const toCryptoCurr = document.querySelector(".to select")
+
+const message = document.querySelector(".message");
 
 for (let select of dropdown) {
     for (let cryptoCurr in cryptoList) {
@@ -22,25 +27,47 @@ for (let select of dropdown) {
     }
 }
 
-btn.addEventListener("click")
 
-let amount = document.querySelector(".amount imput")
+const updateExchangeRate = async () =>{
+    let amount = document.querySelector(".amount input");
+    let amountValue = amount.value;
+}
+ 
+btn.addEventListener("click", async (evt) =>{
+evt.preventDefault();
+let amount = document.querySelector(".amount input")
 let amountValue = amount.value;
-if (amountValue === "" || amountValue < 0){
+if (amountValue === "" || amountValue < 0.00000001) {
     amountValue = 1;
- amount.value = "1";
+    amount.value = "1";
 }
 
-console.log(fromCryptoCurr.value, toCryptoCurr.value);
+// console.log( );
+    let a = fromCryptoCurr [fromCryptoCurr.selectedIndex].text.toLowerCase();
+    console.log(a);
 
-const URL = `${BASE_URL}/$fromCryptoCurr.value.toLowerCase().json`
+// [fromCryptoCurr.selectedIndex].text.toLowerCase()
+
+const URL = `${BASE_URL}/${a}`;
 
 let response = await fetch(URL);
 let data = await response.json();
-let rates = (data[fromCryptoCurr.value.toLowerCase()])
-// console.log(rates);
+let priceUsd = (data[fromCryptoCurr.value.toLowerCase()][toCryptoCurr.value.toLowerCase()]);
+console.log(rates);
+let finalAmt = amountValue * priceUsd;
 
 
+message.innerHTML = `${amountValue} ${fromCryptoCurr} = ${finalAmt} ${toCryptoCurr.value}`
+});
 
 
+const cryptoList = {
+    ETH: "Ethereum",
+    BTC: "Bitcoin",
+    USDT: "Tether",
+    BNB: "BNB"
+};
 
+for (let key in cryptoList) {
+    console.log(cryptoList[key].toLowerCase());
+}
