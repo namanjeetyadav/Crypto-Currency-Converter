fddbd02787bf3a0f49e484cde36ca283aa21f7f7
namanjeet <namanshuttler@gmail.com>
2024-08-03 00:51:56 +0530

    03.08.24

--- 18_Crypto_converter_app.js
+++ 18_Crypto_converter_app.js
@@ -1,7 +1,5 @@
 const BASE_URL = "https://api.coincap.io/v2/assets"
 
-// https://api.coincap.io/v2/assets/ethereum
-
 const dropdown = document.querySelectorAll(".all-select select")
 
 const btn = document.querySelector("form button")
@@ -33,72 +31,59 @@ const updateExchangeRate = async () => {
     let amountValue = amount.value;
 }
 
-btn.addEventListener("click", async (evt) =>{
-evt.preventDefault();
-let amount = document.querySelector(".amount input")
-let amountValue = amount.value;
-if (amountValue === "" || amountValue < 0.00000001) {
-    amountValue = 1;
-    amount.value = "1";
-}
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input")
+    let amountValue = amount.value;
+    if (amountValue === "" || amountValue < 0.00000001) {
+        amountValue = 1;
+        amount.value = "1";
+    }
 
 
-    let a = fromCryptoCurr [fromCryptoCurr.selectedIndex].text.toLowerCase();
-    let b = toCryptoCurr [toCryptoCurr.selectedIndex].text.toLowerCase();
+    let a = fromCryptoCurr.value.toLowerCase();
+    let b = toCryptoCurr.value.toLowerCase();
     console.log(a, b);
 
 
 
-const URL = `${BASE_URL}`;
+    const URL = `${BASE_URL}`;
 
 
-let response = await fetch(URL);
-let data = await response.json();
-let priceUsd = data.data.priceUsd;
-let priceUsd2 = data.data.priceUsd;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let keyToCheck = a;
+    let keyToCheck2 = b;
+    // console.log(data.data.find(obj => obj.id === keyToCheck))
+    // console.log(data.data.find(obj => obj.id === keyToCheck2))
 
-console.log(`Price in USD: ${priceUsd}`);
-let finalAmt = amountValue * priceUsd / priceUsd2;
+    //  code start here 
+
+    let obj1 = data.data.find(obj => obj.id === keyToCheck);
+    let obj2 = data.data.find(obj => obj.id === keyToCheck2);
+
+    if (obj1) {
+        priceUsd1 = parseFloat(obj1.priceUsd)
+        console.log(priceUsd1);
+    }
+
+    if (obj2) {
+         priceUsd2 = parseFloat(obj2.priceUsd)
+        console.log(priceUsd2);
+    }
+
+    //  code end here 
+
+    //
+    // let priceUsd2 = data.data.priceUsd;
+    // console.log(`Price in USD: ${priceUsd}`);
+    // let finalAmt = amountValue * priceUsd / priceUsd2;
 
 
-message.innerHTML = `${amountValue} ${fromCryptoCurr.value} = ${finalAmt} ${toCryptoCurr.value}`
+    // console.log(`Price in USD: ${priceUsd}`);
+    let finalAmt = amountValue * priceUsd1 / priceUsd2;
+    console.log(finalAmt)
+
+
+    message.innerHTML = `${amountValue} ${fromCryptoCurr.value} = ${finalAmt} ${toCryptoCurr.value}`
 });
-
-// btn.addEventListener("click", async (evt) => {
-//     evt.preventDefault();  // Restrict the default behaviour of form to refresh the page while submission.
-//     let amount = document.querySelector(".amount input");
-//     let amountValue = parseFloat(amount.value);
-//     if (isNaN(amountValue) || amountValue < 0.00000001) {
-//         amountValue = 1;
-//         amount.value = "1";
-//     }
-
-//     let fromCrypto = fromCryptoCurr[fromCryptoCurr.selectedIndex].value.toLowerCase();
-//     let toCrypto = toCryptoCurr[toCryptoCurr.selectedIndex].value.toLowerCase();
-//     console.log(fromCrypto, toCrypto);
-
-//     try {
-//         const URL = `${BASE_URL}/${fromCrypto}.json`;
-//         const URL2 = `${BASE_URL}/${toCrypto}.json`;
-
-//         let response = await fetch(URL);
-//         if (!response.ok) throw new Error(`Error fetching data for ${fromCrypto}`);
-//         let data = await response.json();
-//         let priceUsd = data.data.priceUsd;
-
-//         let response2 = await fetch(URL2);
-//         if (!response2.ok) throw new Error(`Error fetching data for ${toCrypto}`);
-//         let data2 = await response2.json();
-//         let priceUsd2 = data2.data.priceUsd;
-
-//         console.log(`Price in USD: ${priceUsd}`);
-//         console.log(`Price in USD: ${priceUsd2}`);
-//         let finalAmt = amountValue * (priceUsd / priceUsd2);
-
-//         message.innerHTML = `${amountValue} ${fromCryptoCurr.value} = ${finalAmt} ${toCryptoCurr.value}`;
-//     } catch (error) {
-//         console.error(error);
-//         message.innerHTML = `An error occurred: ${error.message}`;
-//     }
-// });
-
